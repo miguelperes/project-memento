19f1d19129fe8e420c0a900e5726fdce69383cfb
Allan <allanbrados@gmail.com>
2016-10-24 19:09:17 -0200

    Creates Input Wrapper

---
+++ Assets/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    public bool testevar;
+
+	public virtual bool cta(int i)
+    {
+        if(i == 1)
+            testevar = true;
+        else
+            testevar = false;
+        return testevar;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            cta(1);
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/Editor/Test_InputWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using NSubstitute;
+
+[TestFixture]
+public class Test_InputManager{
+
+    [TearDown]
+    public void tearDown()
+    {
+        InputWrapper.clearSimulatedInputs();
+    }
+
+    [Test]
+    public void testAddOneSimulatedKeyOnList()
+    {
+        string key = "Fire1";
+
+        InputWrapper.addSimulatedKey(key);
+
+        Assert.IsTrue(InputWrapper.getActiveSimulatedKeys().Contains(key));
+    }
+
+    [Test]
+    public void testRemoveKeyFromEmptyHash()
+    {
+        InputWrapper.getButtonDown("Fire1");
+    }
+}
