93dbf80f262cd05913feea303806ce697e357060
Allan <allanbrados@gmail.com>
2016-10-24 22:40:06 -0200

    Creating the custom editor for the InputScripter

---
+++ Assets/Editor/Input Scripter/CustomInputScripter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(InputScripter))]
+public class CustomInputScripter : Editor {
+    InputScripter it;
+
+    void OnEnable()
+    {
+        it = target as InputScripter;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GUILayout.Label("" + it.input.Count);
+
+        if(GUILayout.Button("New"))
+        {
+            it.addNewInput();
+        }
+
+        for(int i = 0; i < it.input.Count; i++)
+        {
+            //GUILayout.Space(5);
+
+            //GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal("box");
+
+            it.input[i].name = GUILayout.TextField(it.input[i].name, GUILayout.ExpandWidth(true));
+
+            if(GUILayout.Button("X", GUILayout.Width(Screen.width * 0.1f)))
+            {
+                it.removeInput(i);
+                return;
+            }
+
+            GUILayout.EndHorizontal();
+            //GUILayout.EndVertical();
+
+        }
+
+        GUILayout.Space(20);
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/Input Scripter/InputScripter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[System.Serializable]
+public class InputType
+{
+    public string name = string.Empty;
+    public Color bg = Color.green;
+}
+
+public class InputScripter : MonoBehaviour {
+
+    public List<InputType> input = new List<InputType>();
+	
+    public void addNewInput()
+    {
+        input.Add(new InputType());
+    }
+
+    public void removeInput(int index)
+    {
+        input.RemoveAt(index);
+    }
+}
