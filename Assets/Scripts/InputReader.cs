99a8d743d811aeb13f389de7503b0e4a7cdbb145
Miguel Péres <foxmpc@gmail.com>
2016-10-31 16:54:20 -0200

    Add script for player movement and input processing (player only); Add a integration test for player movement.

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputReader : MonoBehaviour {
+
+	[SerializeField] PlayerMovement playerMovement = null;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		playerMovement.move (InputManager.getAxis ("Horizontal"), InputManager.getAxis ("Vertical"));	
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody rigid;
+
+	[SerializeField] float speed = 2.0F;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void move(float x, float y) {
+		rigid.velocity = new Vector3 (x * speed, rigid.velocity.y, y * speed);
+	}
+}
